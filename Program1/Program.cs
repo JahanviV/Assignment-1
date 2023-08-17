14e442d767db63df099de759a60c441d0c8279d7
Kapil <Kapil@KAPIL>
2023-08-17 10:03:23 +0530

    Add project files.

---
+++ Program1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 4, 6, 8, 10 };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+       
+            Console.WriteLine("The sum of the elements is: " + sum);
+        }
+    }
+}
